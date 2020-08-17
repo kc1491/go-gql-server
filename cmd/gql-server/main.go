5b33c51f7caacbdc2fd5e66e7f8146304284a827
Casey St.Fleur <kc1491@msn.com>
2020-08-16 23:56:08 -0400

    Initial Build

---
+++ cmd/gql-server/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/kc1491/go-gql-server/pkg/server"
+)
+
+func main() {
+	server.Run()
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kc1491/go-gql-server
+
+go 1.15
+
+require github.com/gin-gonic/gin v1.6.3
